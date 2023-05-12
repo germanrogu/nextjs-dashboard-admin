b6fe0a41cf404ae2d26fa93c0038f9c83e23413c
germanrogu <german.rodriguezgu@gmail.com>
2023-05-11 22:06:15 -0500

    Main Layout and config

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,21 @@
+import Nav from '@/common/Nav';
+import Header from '@/components/Header';
+import React from 'react';
+
+const MainLayout = ({ children }) => {
+  return (
+    <>
+      <div className="min-h-full">
+        <Header />
+        <Nav />
+        <main>
+          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+            {children}
+          </div>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default MainLayout;
